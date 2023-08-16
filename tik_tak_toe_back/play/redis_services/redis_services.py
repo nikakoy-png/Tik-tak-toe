e4be6bfbfa39e2e02b6f4d8d68f8d20ec0917e0a
nikakoy-png <64211546+nikakoy-png@users.noreply.github.com>
2023-08-16 22:12:07 +0200

    [UPD]: Update services Redis

--- tik_tak_toe_back/play/redis_services/redis_services.py
+++ tik_tak_toe_back/play/redis_services/redis_services.py
@@ -7,6 +7,8 @@ async def start_turn_timer(game_id, player_id, timeout=60):
     redis = await get_redis_connection()
     expiration = (datetime.now() + timedelta(seconds=timeout)).timestamp()
     await redis.set(f"turn_timer:{game_id}:{player_id}", expiration)
+    redis.close()
+    await redis.wait_closed()
 
 
 async def get_next_player(game_id, players):
@@ -19,12 +21,16 @@ async def get_next_player(game_id, players):
         next_turn_index = (current_turn_index + 1) % len(players)
         current_turn = players[next_turn_index]
     await redis.set(f"current_turn:{game_id}", current_turn)
+    redis.close()
+    await redis.wait_closed()
     return current_turn
 
 
 async def is_turn_expired(game_id, player_id):
     redis = await get_redis_connection()
     expiration = await redis.get(f"turn_timer:{game_id}:{player_id}")
+    redis.close()
+    await redis.wait_closed()
     if expiration:
         return float(expiration) < datetime.now().timestamp()
     return False
