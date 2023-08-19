d93a8376293387855c69f361c655e6073732a53d
nikakoy-png <64211546+nikakoy-png@users.noreply.github.com>
2023-08-19 20:11:00 +0200

    [UPD]: Update DTO models

--- tik_tak_toe_front/src/app/models/login.dto.ts
+++ tik_tak_toe_front/src/app/models/login.dto.ts
@@ -1,4 +1,4 @@
 export class LoginDto {
-  useraname: string;
-  password: string;
+  username!: string;
+  password!: string;
 }
--- tik_tak_toe_front/src/app/models/registration.dto.ts
+++ tik_tak_toe_front/src/app/models/registration.dto.ts
@@ -1,8 +1,8 @@
 export class RegistrationDto {
-  username: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  password: string;
-  passwordConfirm: string;
+  username!: string;
+  email!: string;
+  firstName!: string;
+  lastName!: string;
+  password!: string;
+  passwordConfirm!: string;
 }
