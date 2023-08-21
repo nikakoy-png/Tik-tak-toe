e7b5d6c0b7e1affd5b0d218775bfc4bad6f6f089
nikakoy-png <64211546+nikakoy-png@users.noreply.github.com>
2023-08-21 19:38:34 +0200

    [UPD]: Replace pulling socket connection on WebSocket

--- tik_tak_toe_front/src/app/search/search.component.ts
+++ tik_tak_toe_front/src/app/search/search.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SearchSocketService} from "../search-socket.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-search',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+  socketUrl = '';
+  typePlay = '';
 
+  constructor(
+    private socketService: SearchSocketService,
+    private route: ActivatedRoute
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.typePlay = params['play_type'];
+      console.log(this.typePlay)
+    });
+    this.socketUrl = `ws://localhost:8000/ws/search-play/${this.typePlay}/`;
+    this.socketService.connectToSocketServer(this.socketUrl);
+    this.socketService.onMessageReceived((msg: string) => {
+      console.log(msg);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.socketService.disconnectFromSocketServer();
+  }
 }
