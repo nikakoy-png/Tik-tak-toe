469f94194a6c5c1bd5ac903f68f66ce926d3e492
nikakoy-png <64211546+nikakoy-png@users.noreply.github.com>
2023-09-04 15:45:39 +0200

    [INIT]: Init new component for {user} objects

---
+++ tik_tak_toe_front/src/app/user/user.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+  @Input() User: any | null;
+}
